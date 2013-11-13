813b6fc9c538e121b032508f31bdbd6ff951915c
Michael Webb <mike_webb56@hotmail.com>
2013-11-13 10:26:09 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ECommFtw::Application.config.secret_token = 'a86435e0dc27bdb712da4ca88a5d2dcfe7b3e7683482a6bec93504c21d2cbc8b5781b2a8a757b0e133fa6bdeb9b219e06cf4a1766ad31a1c1ddf8c4f26f1ca59'
